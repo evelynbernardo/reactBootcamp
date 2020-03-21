9a84d70df4193fdd55de5c6a3e786bd81a370456
evelynbernardo <bernardoevelynbarros@gmail.com>
2020-03-21 09:33:08 -0300

    adding the project

---
+++ day03/formulario-cep/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AddressForm from './Components/form'
+
+function App() {
+  return (
+    <div className="App">
+        <AddressForm/>
+    </div>
+  );
+}
+
+export default App;
