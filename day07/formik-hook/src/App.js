7c682886170ed201c55c50bd496fa29a69cb7c59
evelynbernardo <bernardoevelynbarros@gmail.com>
2020-04-01 21:22:26 -0300

    creating a form using formik

---
+++ day07/formik-hook/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Form from './Components/form'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Formulário</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
